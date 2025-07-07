3711d522778211d3f0cdd0e154a836878b5d9bf3
sayem178 <musaddiksayem@gmail.com>
2025-07-08 00:11:48 +0600

    Add player struct definition and initialization function

--- player.h
+++ player.h
@@ -1,1 +1,14 @@
-#include <stdio.h>
+#ifndef PLAYER_H
+#define PLAYER_H
+
+typedef struct
+{
+    int x, y;
+    int hp;
+    int xp;
+    int level;
+} Player;
+
+void initPlayer(Player *p)
+
+#endif // !PLAYER_H
