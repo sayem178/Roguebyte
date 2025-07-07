97552c4b78519e2dcbcb1ddd5bea7f52d4112c6f
sayem178 <musaddiksayem@gmail.com>
2025-07-08 00:27:26 +0600

    Refactor player struct definition for clarity and add comments

--- player.h
+++ player.h
@@ -1,14 +1,15 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-typedef struct
-{
-    int x, y;
-    int hp;
-    int xp;
-    int level;
+// Player info
+typedef struct {
+    int x, y;      // Position on map
+    int hp;        // Health
+    int xp;        // Experience
+    int level;     // Level
 } Player;
 
+// Set default values for player
 void initPlayer(Player *p);
 
-#endif // !PLAYER_H
+#endif // PLAYER_H
